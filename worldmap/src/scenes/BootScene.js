36098dc1b1f4ef91217304b039593bce552b85a1
QiQi <qiqi@daotu.game>
2026-03-04 19:03:04 +0700

    feat(worldmap): switch to real tiled-json map with tileset texture and collision

--- worldmap/src/scenes/BootScene.js
+++ worldmap/src/scenes/BootScene.js
@@ -4,18 +4,16 @@ export class BootScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.json('south_meta', './src/data/south_continent.meta.json');
+    this.load.image('nanzhan_tiles', './assets/tilesets/nanzhan_tileset.svg');
+    this.load.tilemapTiledJSON('nanzhan_map', './assets/tilemaps/nanzhan_worldmap.json');
     this.load.json('south_poi', './src/data/south_poi.json');
     this.load.json('south_encounters', './src/data/south_encounters.json');
-    this.load.json('south_factions', './src/data/south_factions.json');
   }
 
   create() {
     this.scene.start('SouthContinentScene', {
-      meta: this.cache.json.get('south_meta'),
       poi: this.cache.json.get('south_poi'),
-      encounters: this.cache.json.get('south_encounters'),
-      factions: this.cache.json.get('south_factions')
+      encounters: this.cache.json.get('south_encounters')
     });
   }
 }
